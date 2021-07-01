f15c03cd2fc458cb2a0c77fae25706bde2144ada
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-01 23:17:59 +0545

    Add Basic UI using JSX (Javascript + HTML hybrid for running JS using `{}`)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <h1>React Firebase TODO App 🚀</h1>
+
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Wake up at 5AM</li>
+        <li>Workout till 5:30</li>
+        <li>Fresh till 6:00</li>
+      </ul>
     </div>
   );
 }
