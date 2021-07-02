1bf94037dffc6df52d6f4d7bcb09b171c42aa583
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-02 14:12:42 +0545

    Add `{ List, ListItem, ListItemText }` from materail-ui/core for the beautiful List ✅

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 import './Todo.css';
 
 function Todo(props) {
   return (
-    <div>
-      <li>{props.text}</li>
-    </div>
+    <List>
+      <ListItem>
+        <ListItemText primary={props.text} secondary="Dummy deadline ⏰" />
+      </ListItem>
+    </List>
   );
 }
 
