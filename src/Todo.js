7a4cc6dd8c5d76aa0cf7e42e9e86954887216ec0
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-03 14:09:28 +0545

    Add the `Modal` and `useState` to `handleOpen` `setOpen(true/false)` of the modal

--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import React, { useState } from 'react';
+import { List, ListItem, ListItemText, Modal } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import './Todo.css';
 import db from './firebase';
 
 function Todo(props) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
+
   return (
-    <List>
-      <ListItem>
-        <ListItemText primary={props.todo.todo} secondary="Dummy deadline ⏰" />
-      </ListItem>
-      <DeleteForeverIcon
-        onClick={(event) => db.collection('todos').doc(props.todo.id).delete()}
-      />
-    </List>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div>
+          <h1>I am a modal</h1>
+          <button onClick={(e) => setOpen(false)}></button>
+        </div>
+      </Modal>
+      <List>
+        <ListItem>
+          <ListItemText
+            primary={props.todo.todo}
+            secondary="Dummy deadline ⏰"
+          />
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>Edit</button>
+        <DeleteForeverIcon
+          onClick={(event) =>
+            db.collection('todos').doc(props.todo.id).delete()
+          }
+        />
+      </List>
+    </>
   );
 }
 
