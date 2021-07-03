b0fbd54ea19ec5b6b2dd4208384d7187e7b5a306
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-03 09:37:42 +0545

    Add Delete todos functionality by adding and passing the array of objects `{ id, todo }` onClick Button event from the `db.collection('todos')` by the `todo.id`

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemText, Button } from '@material-ui/core';
 import './Todo.css';
+import db from './firebase';
 
 function Todo(props) {
   return (
     <List>
       <ListItem>
-        <ListItemText primary={props.text} secondary="Dummy deadline ⏰" />
+        <ListItemText primary={props.todo.todo} secondary="Dummy deadline ⏰" />
       </ListItem>
+      <Button
+        onClick={(event) => db.collection('todos').doc(props.todo.id).delete()}
+      >
+        ❌ Delete
+      </Button>
     </List>
   );
 }
