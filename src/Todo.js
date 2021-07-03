a7cbf35928471a77dcb4f4d157d36d421199bf97
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-03 14:26:46 +0545

    Style the Modal box by `import { makeStyles } from material-ui/core/styles`

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import { List, ListItem, ListItemText, Modal } from '@material-ui/core';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { makeStyles } from '@material-ui/core/styles';
 import './Todo.css';
 import db from './firebase';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function Todo(props) {
+  const classes = useStyles();
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
@@ -18,7 +31,7 @@ function Todo(props) {
   return (
     <>
       <Modal open={open} onClose={(e) => setOpen(false)}>
-        <div>
+        <div className={classes.paper}>
           <h1>I am a modal</h1>
           <button onClick={(e) => setOpen(false)}></button>
         </div>
